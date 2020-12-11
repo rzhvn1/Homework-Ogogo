7ad7f632a9aa005c6e8fa206a897b7635338918a
rzhvn1 <erzhanmuratov99@gmail.com>
2020-12-11 23:59:37 +0600

    Serj and Dima are playing cards and Nina is watching

---
+++ Serj and Dima/serj_and_dima.py
@@ -0,0 +1,16 @@
+n = int(input('Please input how many cards on the table: '))
+cards = list(map(int, input('Please input the values on the cards: ').split()))
+serj = 0
+dima = 0
+i = 0
+while i < n:
+    scale = max(cards[0], cards[-1])
+    if i % 2 == 0:
+        serj += scale
+    else:
+        dima += scale
+    i += 1
+    cards.remove(scale)
+
+print(serj, dima)
+
